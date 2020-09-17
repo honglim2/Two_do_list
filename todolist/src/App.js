41afe4c1eb0535829ecea79e312b7e5bbe224739
HongJunHyeok <edb1631@naver.com>
2020-09-17 13:05:44 +0900

    [UPDATE]
    css&js추가

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -4,7 +4,9 @@ import Todo from "./components/Todo";
 function App() {
     return (
         <div className="App">
-            <Todo></Todo>
+            <Todo form={<div>이쪽은 form을 보여주는 부분이다.</div>}>
+                이쪽은 children을 보여주는 부분이다.
+            </Todo>
         </div>
     );
 }
