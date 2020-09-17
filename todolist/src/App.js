8241256821c5b7e0117838348ecfd1de255fe35f
HongJunHyeok <edb1631@naver.com>
2020-09-17 15:29:39 +0900

    [UPDATE]
    기능 추가

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -2,11 +2,15 @@ import React, { useState } from "react";
 import "./App.css";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
+import TodoItemList from "./components/TodoItemList";
+import TodoItem from "./components/TodoItem";
 
 function App() {
     return (
         <div className="App">
-            <Todo form={<Form></Form>}></Todo>
+            <Todo form={<Form></Form>}>
+                <TodoItemList></TodoItemList>
+            </Todo>
         </div>
     );
 }
---
+++ todolist/src/components/TodoItemList.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import TodoItem from "./TodoItem";
+
+class TodoItemList extends Component {
+    render() {
+        const { todos, onToggle, onRemove } = this.props;
+        return (
+            <div>
+                <TodoItem text="안녕"></TodoItem>
+                <TodoItem text="안녕"></TodoItem>
+                <TodoItem text="안녕"></TodoItem>
+            </div>
+        );
+    }
+}
+
+export default TodoItemList;
