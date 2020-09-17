a23d86820da23a26711700def68f81aac154071b
HongJunHyeok <edb1631@naver.com>
2020-09-17 13:28:41 +0900

    [UPDATE]
    input button 추가

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
 import Todo from "./components/Todo";
+import Form from "./components/Form";
+
 function App() {
     return (
         <div className="App">
-            <Todo form={<div>이쪽은 form을 보여주는 부분이다.</div>}>
-                이쪽은 children을 보여주는 부분이다.
-            </Todo>
+            <Todo form={<Form></Form>}></Todo>
         </div>
     );
 }
